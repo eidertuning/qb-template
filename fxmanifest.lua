df0df9ff8dad63f96c8c26336cf8ba2deae64cae
CodeEider <52119762+eidertuning@users.noreply.github.com>
2022-11-02 08:31:31 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'evanillaa'
+description 'QB-Template https://github.com/evanillaa'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+	'html/*.html',
+	'html/*.js',
+	'html/*.png',
+	'html/*.css',
+}
+
+lua54 'yes'
